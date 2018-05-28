ddbe52c7f0f4709bc8c81ba1786d2e6bbfe1ad1e
Dyan Galih <dyan.galih@gmail.com>
2018-05-28 08:51:41 +0700

    setup unit test core and sample for tdd

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,43 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Faker\Factory as Faker;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication,DatabaseMigrations, DatabaseTransactions;
+
+
+    protected $faker;
+    /**
+     * Set up the test
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->faker = Faker::create();
+    }
+
+    /**
+     * Create Aplication set for fake connection
+     *
+     * @return void
+     */
+    public function createApplication(){
+        putenv('DB_CONNECTION=sqlite');
+        $app = require __DIR__ . '/../bootstrap/app.php';
+        $app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
+        return $app;
+    }
+
+    /**
+     * Reset the migrations
+     */
+    public function tearDown()
+    {
+        $this->artisan('migrate:reset');
+        parent::tearDown();
+    }
 }
