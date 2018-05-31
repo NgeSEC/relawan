a51643f7c4a90c1c96cf0864fd0af71ceabe6905
djenova <mr.dyrgha@gmail.com>
2018-05-31 17:26:56 +0700

    percobaan Pertama OK

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    POSKO.ID
                 </div>
 
                 <div class="links">
