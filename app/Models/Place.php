96d027323c23d69b1d5750c107b32bbcbdeaab9f
Dyan Galih <dyan.galih@gmail.com>
2018-06-16 12:15:17 +0700

    create add place and update place out from loop

--- app/Models/Place.php
+++ app/Models/Place.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Auth;
 use Illuminate\Database\QueryException;
 
 class Place extends Content
@@ -18,8 +17,56 @@ class Place extends Content
         $this->objTimezone = new TimeZone;
     }
 
+    public function addPlace($placeProperties, $code, $keyword, $listPlace, $timezone, $owner_id, $user_id)
+    {
+        $objNewContent = new \StdClass;
+        $objNewContent->title = $placeProperties['Name'];
+        $objNewContent->code = $code;
+        $objNewContent->description = $placeProperties['description'];
+        $objNewContent->keyword = $keyword;
+        $objNewContent->og_title = $placeProperties['Name'];
+        $objNewContent->og_description = $placeProperties['description'];
+        $objNewContent->default_image = '1';
+        $objNewContent->status_id = '2';
+        $objNewContent->language_id = '1';
+        $objNewContent->publish_date = date_format_to_utc();
+        $objNewContent->additional_info = $listPlace;
+        $objNewContent->content = '';
+        $objNewContent->time_zone_id = $timezone->id;
+        $objNewContent->owner_id = $owner_id;
+        $objNewContent->user_id = $user_id;
+        
+        return $this->addContent($objNewContent);
+    }
+
+    public function updatePlace($content, $placeProperties, $code, $keyword, $listPlace, $timezone, $owner_id, $user_id){
+        try {
+            $content->title = $placeProperties['Name'];
+            $content->code = $code;
+            $content->description = $placeProperties['description'];
+            $content->keyword = $keyword;
+            $content->og_title = $placeProperties['Name'];
+            $content->og_description = $placeProperties['description'];
+            $content->default_image = '1';
+            $content->status_id = '2';
+            $content->language_id = '1';
+            $content->publish_date = date_format_to_utc();
+            $content->additional_info = $listPlace;
+            $content->content = '';
+            $content->time_zone_id = $timezone->id;
+            $content->owner_id = $owner_id;
+            $content->user_id = $user_id;
+            $content->save();
+            return true;
+        } catch (QueryException $e) {
+            report($e);
+            return false;
+        }
+    }
+
     public function addBulkPlace($dataPlace, $user_id, $owner_id, $timezone)
     {
+        
         $result = true;
         $listPlace = $dataPlace['features'];
         $timezone = $this->objTimezone->getOneTimeZoneByName($timezone);
@@ -33,48 +80,14 @@ class Place extends Content
             $placeCoordinate = $listPlace[$i]['geometry']['coordinates'];
 
             if ($content == null) {
-                $objNewContent = new \StdClass;
-                $objNewContent->title = $placeProperties['Name'];
-                $objNewContent->code = $code;
-                $objNewContent->description = $placeProperties['description'];
-                $objNewContent->keyword = $keyword;
-                $objNewContent->og_title = $placeProperties['Name'];
-                $objNewContent->og_description = $placeProperties['description'];
-                $objNewContent->default_image = '1';
-                $objNewContent->status_id = '2';
-                $objNewContent->language_id = '1';
-                $objNewContent->publish_date = date_format_to_utc();
-                $objNewContent->additional_info = json_encode($listPlace[$i]);
-                $objNewContent->content = '';
-                $objNewContent->time_zone_id = $timezone->id;
-                $objNewContent->owner_id = $owner_id;
-                $objNewContent->user_id = $user_id;
-
-                $content = $this->addContent($objNewContent);
+                $content = $this->addPlace($placeProperties, $code, $keyword, json_encode($listPlace[$i]), $timezone, $owner_id, $user_id);
                 if (!$content) {
                     return false;
                 }
             } else {
-                try {
-                    $content->title = $placeProperties['Name'];
-                    $content->code = $code;
-                    $content->description = $placeProperties['description'];
-                    $content->keyword = $keyword;
-                    $content->og_title = $placeProperties['Name'];
-                    $content->og_description = $placeProperties['description'];
-                    $content->default_image = '1';
-                    $content->status_id = '2';
-                    $content->language_id = '1';
-                    $content->publish_date = date_format_to_utc();
-                    $content->additional_info = json_encode($listPlace[$i]);
-                    $content->content = '';
-                    $content->time_zone_id = $timezone->id;
-                    $content->owner_id = $owner_id;
-                    $content->user_id = $user_id;
-                    $content->save();
-                } catch (QueryException $e) {
-                    report($e);
-                    $result = false;
+                $result = $this->updatePlace($content, $placeProperties, $code, $keyword, json_encode($listPlace[$i]), $timezone, $owner_id, $user_id);
+                if(!$result){
+                    return false;
                 }
             }
 
@@ -87,24 +100,25 @@ class Place extends Content
 
             $placeGeometry['content_id'] = $content->id;
             $placeGeometry['user_id'] = $user_id;
-            
+
             $placeGeometry = $this->objContentGeometry->addContentGeometry((Object) $placeGeometry);
-            if(!$placeGeometry){
+            if (!$placeGeometry) {
                 return false;
-            }else{
+            } else {
                 $placeCoordinate['geometry_id'] = $placeGeometry->id;
                 $placeCoordinate['user_id'] = $user_id;
                 $geometryCoordinate = $this->objContentGeometryCoordinate->addGeometryCoordinate($placeCoordinate);
-                if(!$geometryCoordinate){
+                if (!$geometryCoordinate) {
                     return false;
                 }
             }
-            
+
         }
         return $content;
     }
 
-    public function getPlaceById($id){
+    public function getPlaceById($id)
+    {
         return $this->find($id);
     }
 
