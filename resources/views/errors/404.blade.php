999c2b538b4e0c92f9d825d7cbaf70bece920a62
Banu Antoro <banumelody@gmail.com>
2018-06-29 03:37:38 +0700

    [Added] 404 page
    Menambahkan halaman 404.

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+    <section class="content">
+        <div class="container">
+            <h2>SORRY, THE PAGE YOU MEAN IS NOT HERE</h2>
+            <h3>Maybe that will apply also to find your life partner. Please try again in another chance.</h3>
+        </div>
+    </section>
+@endsection
