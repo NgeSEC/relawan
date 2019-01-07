a3cef3fd4b17df9fa09149a040676c6ec5119b06
Dyan Galih <dyan.galih@gmail.com>
2019-01-08 01:12:29 +0700

    Direct parse posko data direct into blade without json

--- app/Http/Controllers/PoskoController.php
+++ app/Http/Controllers/PoskoController.php
@@ -21,58 +21,6 @@ class PoskoController extends Controller
     
     public function index()
     {
-        $poskos = $this->placeService->getAllPosko();
-        
-        if(count($poskos)>0) {
-    
-            $result = new \StdClass();
-            $result->type = 'FeatureCollection';
-            
-            $crsGroup = new \StdClass();
-            $crsGroup->type = 'name';
-            
-            $crsPropertiesGroup = new \StdClass();
-            $crsPropertiesGroup->name = 'urn:ogc:def:crs:OGC:1.3:CRS84';
-            $crsGroup->properties = $crsPropertiesGroup;
-            
-            $result->crs = $crsGroup;
-            
-            $featuresList = [];
-            
-            for ($i = 0; $i < count($poskos); $i++) {
-                $featureItem = new \StdClass();
-                $featureItem->type = 'Feature';
-                
-                $featureItemProperties = new \StdClass();
-                $featureItemProperties->Name = $poskos[$i]->title;
-                $featureItemProperties->description = $poskos[$i]->content;
-                $featureItemProperties->timestamp = null;
-                $featureItemProperties->begin = null;
-                $featureItemProperties->end = null;
-                $featureItemProperties->altitudeMode = null;
-                $featureItemProperties->tessellate = -1;
-                $featureItemProperties->extrude = 0;
-                $featureItemProperties->visibility = -1;
-                $featureItemProperties->drawOrder = null;
-                $featureItemProperties->gx_media_links = null;
-                $featureItem->properties = $featureItemProperties;
-                
-                
-                $featureItemGeometry = new \StdClass();
-                $featureItemGeometry->type = $poskos[$i]->contentGeometry[0]->type;
-                
-                $featureItemGeometryCoordinate = [];
-                $featureItemGeometryCoordinate[] = $poskos[$i]->contentGeometry[0]->coordinate[0]->longitude;
-                $featureItemGeometryCoordinate[] = $poskos[$i]->contentGeometry[0]->coordinate[0]->latitude;
-                $featureItemGeometry->coordinates = $featureItemGeometryCoordinate;
-                $featureItem->geometry = $featureItemGeometry;
-    
-                $featuresList[] = $featureItem;
-            }
-    
-            $result->features = $featuresList;
-        }
-        
-        return (Array)$result;
+        return $this->placeService->getPoskoLeaflet();
     }
 }
